dc784ea37049ba503700f547e90fa0156e2ab3a9
Entrar <alberto.melian103@alu.ulpgc.es>
2022-03-22 11:33:21 +0000

    Added JSON content loading

--- ArticleLoader.js
+++ ArticleLoader.js
@@ -1,10 +1,16 @@
 $(document).ready(function(){
-    $("title").text("llafkj");
+    var title;
+    var content;
+    $.getJSON("assets/test.json", function (data) {
+        title=data.title;
+        content=data.content;
+    });
+    $("title").text(title);
     $("#header").load('assets/navbar.html');
-    $("#article").load('assets/articlePage.html');
-    $(".longArticle > h1").text('Montañón');
-    $("#imgLongArticle").attr("src","https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.ngenespanol.com%2Fdato-" +
-        "dia%2Fcual-es-considerada-la-montana-perfecta%2F&psig=AOvVaw3Pw3F-e318oAmX6W9x0GWh&ust=1647943799106000&source=" +
-        "images&cd=vfe&ved=0CAgQjRxqFwoTCLDltZT71vYCFQAAAAAdAAAAABAE");
+    $("#article").load('assets/articlePage.html', function () {
+        $(".longArticle h1").text(title);
+        $("#imgLongArticle").attr("src","https://dam.ngenespanol.com/wp-content/uploads/2019/06/montana-perfecta_Matterhorn_-770x395.png");
+        $(".longArticle p").text(content);
+    });
     $("#footer").load('assets/footerTestHTML.html');
 });
