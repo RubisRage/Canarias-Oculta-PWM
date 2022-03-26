6d4b5da331371a519e6588c186e90a538f9a9e28
Aeron <Aeronpsaro@gmail.com>
2022-03-26 19:18:31 +0000

    Integrated jQuery with responsive design

--- homeLoader.js
+++ homeLoader.js
@@ -1,19 +1,34 @@
 $(document).ready(function(){
-    var first = true;
+    const carouselArticles = 2;
+    let i = 1;
     $("#header").load('assets/navbar.html');
-    $("#carousel").load('assets/carousel.html');
+    $("#carousel").load('assets/carousel.html', function () {
+        $.getJSON('assets/longTest.json', function(data) {
+            $.each(data, function (key, val) {
+                if (i > carouselArticles) {return;}
+                $(".carousel-inner").append("<article></article>");
+                $("article").last().load('assets/carouselItem.html', function () {
+                    $(".carousel-item").last().children("h2").text(val.title);
+                    $(".carousel-item").last().children("p").text(val.content.slice(0, 300) + "...");
+                    $(".carousel-item").last().children("img").attr("src", val.imageURL);
+                    $(".carousel-item").last().children("img").attr("alt", val.altText);
+                });
+                i++;
+            });
+        });
+    });
     $.getJSON('assets/longTest.json', function(data) {
+        i=1
         $.each(data, function (key, val) {
             $("main").append("<article></article>");
-            if (first) {
+            if (i==carouselArticles+1) {
                 $("main article").last().load('assets/articleResume.html', function () {
                     $(".articleResume").last().children("h1").text(val.title);
                     $(".articleResume").last().children("p").text(val.content.slice(0,300)+"...");
                     $(".articleResume").last().children("img").attr("src", val.imageURL);
                     $(".articleResume").last().children("img").attr("alt", val.altText);
                 });
-                first = false;
-            } else {
+            } else if (i>carouselArticles+1) {
                 $("main article").last().load('assets/articleResumeSmall.html', function () {
                     $(".articleResumeSmall").last().children("h1").text(val.title);
                     $(".articleResumeSmall").last().children("p").text(val.content.slice(0,200)+"...");
@@ -21,6 +36,7 @@ $(document).ready(function(){
                     $(".articleResumeSmall").last().children("img").attr("alt", val.altText);
                 });
             }
+            i++;
         })
     });
     $("#footer").load('assets/footerTestHTML.html');
--- islandLoader.js
+++ islandLoader.js
@@ -3,8 +3,7 @@ $(document).ready(function(){
     $("#header").load('assets/navbar.html');
     $("main #verticalNavBar").load('assets/verticalNavbar.html');
     $.getJSON('assets/longTest.json', function(data) {
-        $("main").append("<article></article>");
-        $("main article").last().load('assets/shortArticle.html', function () {
+        $("#shortArticle").load('assets/shortArticle.html', function () {
             $.each(data, function (key, val) {
                 if (first) {
                         $(".shortArticle").children("p").text(val.content.slice(0,300)+"...");
@@ -12,11 +11,11 @@ $(document).ready(function(){
                         $(".shortArticle").children("img").attr("alt", val.altText);
                     first = false;
                 } else {
-                    $("article").children().append("<section></section>");
-                    $("section").last().load('assets/imgSection.html', function () {
-                        $("section section").last().children("h3").text(val.title);
-                        $("section section").last().children("img").attr("src", val.imageURL);
-                        $("section section").last().children("img").attr("alt", val.altText);
+                    $("#sections").append("<div class='col'></div>");
+                    $(".col").last().load('assets/imgSection.html', function () {
+                        $("section").last().children("h3").text(val.title);
+                        $("section").last().children("img").attr("src", val.imageURL);
+                        $("section").last().children("img").attr("alt", val.altText);
                     });
                 }
             })
